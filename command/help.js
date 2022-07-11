24b5e2ad13078ea33be5d92c724512af6814b96d
andhyca-offc <108988560+andhyca-offc@users.noreply.github.com>
2022-07-11 17:37:50 +0700

    Update help.js

--- command/help.js
+++ command/help.js
@@ -13,173 +13,197 @@ function toCommas(x) {
 }
 
 exports.allMenu = (ucapanWaktu, pushname, mundur, upload, download, ownerName, botName, jam, tanggal, runtime, isOwner, isPremium, sender, limitCount, limit, gcount, glimit, balance, prefix) => {
-    return`${ucapanWaktu} ${pushname !== undefined ? pushname : 'Kak'}
-
-Hitung Mundur Idul Adha
-${mundur}
-
-*STATISTICS*
- • Upload : ${upload}
- • Downloads : ${download}
-
-*BOT INFO*
- • Creator : ${ownerName}
- • Bot Name : ${botName}
- • Time : ${jam}
- • Date : ${tanggal}
- • Runtime :
- ${runtime(process.uptime())}
-
-*USER INFO*
- • Name : ${pushname !== undefined ? pushname : '-'}
- • Status : ${isOwner ? 'Owner' : isPremium ? 'Premium' : 'Free'}
- • Limit : ${isOwner ? '-' : isPremium ? 'Unlimited' : getLimit(sender, limitCount, limit)}
- • Limit Game : ${isOwner ? '-' : cekGLimit(sender, gcount, glimit)}
- • Balance : $${toCommas(getBalance(sender, balance))}
-
-*MAIN MENU*${petik}
- • ${prefix}menu
- • ${prefix}infobot
- • ${prefix}donate
- • ${prefix}dashboard
- • ${prefix}owner
- • ${prefix}cekdrive
- • ${prefix}cekbandwidth
- • ${prefix}cekpremium
- • ${prefix}listpremium
- • ${prefix}listsewa
- • ${prefix}speed
- • ${prefix}runtime
- • ${prefix}listbahasa${petik}
-
-*CONVERTER/TOOLS*${petik} 
- • ${prefix}sticker
- • ${prefix}stickerwm
- • ${prefix}smeme
- • ${prefix}toimg
- • ${prefix}tovideo
- • ${prefix}tomp3
- • ${prefix}ttp
- • ${prefix}attp
- • ${prefix}emojimix
- • ${prefix}nuliskiri
- • ${prefix}nuliskanan
- • ${prefix}foliokiri
- • ${prefix}foliokanan
- • ${prefix}say
- • ${prefix}translate${petik}
-
-*ANONYMOUS CHATS*${petik}
- • ${prefix}anonymous
- • ${prefix}start
- • ${prefix}next
- • ${prefix}stop
- • ${prefix}sendprofile${petik}
-
-*STORE MENU*${petik}
- • ${prefix}list
- • ${prefix}addlist
- • ${prefix}dellist
- • ${prefix}update
- • ${prefix}jeda
- • ${prefix}tambah
- • ${prefix}kurang
- • ${prefix}kali
- • ${prefix}bagi
- • proses < reply chat >
- • done < reply chat >${petik}
-
-*DOWNLOADS MENU*${petik}
- • ${prefix}play
- • ${prefix}ytmp3
- • ${prefix}ytmp4
- • ${prefix}instagram
- • ${prefix}igstory
- • ${prefix}tiktok
- • ${prefix}tiktoknowm
-  • ${prefix}tiktokaudio
- • ${prefix}facebook
- • ${prefix}mediafire${petik}
-
-*GROUP MENU*${petik}
- • ${prefix}afk
- • ${prefix}welcome
- • ${prefix}left
- • ${prefix}setwelcome
- • ${prefix}changewelcome
- • ${prefix}delsetwelcome
- • ${prefix}setleft
- • ${prefix}changeleft
- • ${prefix}delsetleft
- • ${prefix}linkgc
- • ${prefix}setppgc
- • ${prefix}setnamegc
- • ${prefix}setdesc
- • ${prefix}antilink
- • ${prefix}antiwame
- • ${prefix}open
- • ${prefix}close
- • ${prefix}add
- • ${prefix}kick
- • ${prefix}promote
- • ${prefix}demote
- • ${prefix}revoke
- • ${prefix}hidetag
- • ${prefix}checksewa${petik}
-
-*GAME MENU*${petik}
- • ${prefix}tictactoe
- • ${prefix}delttt
- • ${prefix}casino
- • ${prefix}delcasino${petik}
-
-*SEARCH MENU*${petik}
- • ${prefix}lirik
- • ${prefix}grupwa
- • ${prefix}pinterest
- • ${prefix}ytsearch
- • ${prefix}searchbyimage${petik}
-
-*RANDOM MENU*${petik}
- • ${prefix}cecan
- • ${prefix}cogan
- • ${prefix}waifu${petik}
+    return`╭─── 「 SHIROBOT-MD 」
+│ ${ucapanWaktu} ${pushname !== undefined ? pushname : 'Kak'}
+│
+│📤 Upload : ${upload}
+│⏬ Downloads : ${download}
+│
+│
+│✍️ Creator : ${ownerName}
+│🤖 Bot Name : ${botName}
+│🕰️ Time : ${jam}
+│📆 Date : ${tanggal}
+│⏲️ Runtime : ${runtime(process.uptime())}
+│
+│
+│📛 Name : ${pushname !== undefined ? pushname : '-'}
+│🏅 Status : ${isOwner ? 'Owner' : isPremium ? 'Premium' : 'Free'}
+│🎗️ Limit : ${isOwner ? '-' : isPremium ? 'Unlimited' : getLimit(sender, limitCount, limit)}
+│🕹️️ Limit Game : ${isOwner ? '-' : cekGLimit(sender, gcount, glimit)}
+│⚖️ Balance : $${toCommas(getBalance(sender, balance))}
+╰────────────────────
 
 
-*BALANCE MENU*${petik}
- • ${prefix}topglobal
- • ${prefix}toplocal
- • ${prefix}buylimit
- • ${prefix}buyglimit
- • ${prefix}transfer
- • ${prefix}limit
- • ${prefix}balance${petik}
+╭── 「 𝑀𝑎𝑖𝑛 𝑀𝑒𝑛𝑢 」
+│• ${prefix}menu
+│• ${prefix}infobot
+│• ${prefix}donate
+│• ${prefix}dashboard
+│• ${prefix}owner
+│• ${prefix}cekdrive
+│• ${prefix}cekbandwidth
+│• ${prefix}cekpremium
+│• ${prefix}listpremium
+│• ${prefix}listsewa
+│• ${prefix}speed
+│• ${prefix}runtime
+│• ${prefix}listbahasa
+╰──────────
 
-*BAILEYS*${petik}
- • ${prefix}fitnah
- • ${prefix}nowa
- • ${prefix}getquoted
- • ${prefix}fakehidetag
- • ${prefix}react
- • ${prefix}setcmd
- • ${prefix}delcmd${petik}
 
-*OWNERS MENU*${petik}
- • ${prefix}join
- • ${prefix}left
- • ${prefix}self
- • ${prefix}public
- • ${prefix}setprefix
- • ${prefix}setppbot
- • ${prefix}broadcast
- • ${prefix}bcsewa
- • ${prefix}addpremium
- • ${prefix}delpremium
- • ${prefix}addsewa
- • ${prefix}delsewa${petik}`
+╭── 「 𝐶𝑜𝑛𝑣𝑒𝑟𝑡𝑒𝑠/𝑇𝑜𝑜𝑙𝑠 」
+│• ${prefix}sticker
+│• ${prefix}stickerwm
+│• ${prefix}smeme
+│• ${prefix}toimg
+│• ${prefix}tovideo
+│• ${prefix}tomp3
+│• ${prefix}ttp
+│• ${prefix}attp
+│• ${prefix}emojimix
+│• ${prefix}translate
+╰─────────
+
+
+╭── 「 𝑊𝑟𝑖𝑡𝑒 𝑀𝑒𝑛𝑢 」
+│• ${prefix}nuliskiri
+│• ${prefix}nuliskanan
+│• ${prefix}foliokiri
+│• ${prefix}foliokanan
+│• ${prefix}say
+╰──────────
+
+
+╭── 「 𝐴𝑛𝑜𝑛𝑦𝑚𝑜𝑢𝑠 𝐶ℎ𝑎𝑡 」
+│• ${prefix}anonymous
+│• ${prefix}start
+│• ${prefix}next
+│• ${prefix}stop
+│• ${prefix}sendprofile
+╰──────────
+
+
+╭── 「 𝑆𝑡𝑜𝑟𝑒 𝑀𝑒𝑛𝑢 」
+│• ${prefix}list
+│• ${prefix}addlist
+│• ${prefix}dellist
+│• ${prefix}update
+│• ${prefix}jeda
+│• ${prefix}tambah
+│• ${prefix}kurang
+│• ${prefix}kali
+│• ${prefix}bagi
+│• proses < reply chat >
+│• done < reply chat >
+╰──────────
+
+
+╭── 「 𝐷𝑜𝑤𝑛𝑙𝑜𝑎𝑑𝑠 𝑀𝑒𝑛𝑢 」
+│• ${prefix}play
+│• ${prefix}ytmp3
+│• ${prefix}ytmp4
+│• ${prefix}instagram
+│• ${prefix}igstory
+│• ${prefix}tiktok
+│• ${prefix}tiktoknowm
+│• ${prefix}tiktokaudio
+│• ${prefix}facebook
+│• ${prefix}mediafire
+╰─────────
+
+
+╭── 「 𝐺𝑟𝑜𝑢𝑝 𝑀𝑒𝑛𝑢 」
+│• ${prefix}afk
+│• ${prefix}welcome
+│• ${prefix}left
+│• ${prefix}setwelcome
+│• ${prefix}changewelcome
+│• ${prefix}delsetwelcome
+│• ${prefix}setleft
+│• ${prefix}changeleft
+│• ${prefix}delsetleft
+│• ${prefix}linkgc
+│• ${prefix}setppgc
+│• ${prefix}setnamegc
+│• ${prefix}setdesc
+│• ${prefix}antilink
+│• ${prefix}antiwame
+│• ${prefix}open
+│• ${prefix}close
+│• ${prefix}add
+│• ${prefix}kick• ${prefix}promote
+│• ${prefix}demote
+│• ${prefix}revoke
+│• ${prefix}hidetag
+│• ${prefix}checksewa
+╰──────────
+
+
+╭── 「 𝐺𝑎𝑚𝑒 𝑀𝑒𝑛𝑢 」
+│• ${prefix}tictactoe
+│• ${prefix}delttt
+│• ${prefix}casino
+│• ${prefix}delcasino
+╰─────────
+
+
+╭── 「 𝑆𝑒𝑎𝑟𝑐ℎ 𝑀𝑒𝑛𝑢 」
+│• ${prefix}lirik
+│• ${prefix}grupwa
+│• ${prefix}pinterest
+│• ${prefix}ytsearch
+│• ${prefix}searchbyimage
+╰────────────
+
+
+╭── 「 𝑅𝑎𝑛𝑑𝑜𝑚 𝑀𝑒𝑛𝑢 」
+│• ${prefix}cecan
+│• ${prefix}cogan
+│• ${prefix}waifu
+╰────────
+
+
+╭── 「 𝐵𝑎𝑙𝑎𝑛𝑐𝑒 𝑀𝑒𝑛𝑢 」
+│• ${prefix}topglobal
+│• ${prefix}toplocal
+│• ${prefix}buylimit
+│• ${prefix}buyglimit
+│• ${prefix}transfer
+│• ${prefix}limit
+│• ${prefix}balance
+╰─────────
+
+
+╭── 「 𝐵𝑎𝑖𝑙𝑒𝑦𝑠 𝑀𝑒𝑛𝑢 」
+│• ${prefix}fitnah
+│• ${prefix}nowa
+│• ${prefix}getquoted
+│• ${prefix}fakehidetag
+│• ${prefix}react
+│• ${prefix}setcmd
+│• ${prefix}delcmd
+╰─────────
+
+
+╭── 「 𝑂𝑤𝑛𝑒𝑟 𝑀𝑒𝑛𝑢 」
+│• ${prefix}join
+│• ${prefix}left
+│• ${prefix}self
+│• ${prefix}public
+│• ${prefix}setprefix
+│• ${prefix}setppbot
+│• ${prefix}broadcast
+│• ${prefix}bcsewa
+│• ${prefix}addpremium
+│• ${prefix}delpremium
+│• ${prefix}addsewa
+│• ${prefix}delsewa
+╰─────────`
 }
 
 exports.donate = (pushname, ownerNumber) => {
     return`Contact  Owner:
-wa.me/62887435047326 (Owner)`
+wa.me/6286872761910 (Owner)
+wa.me/6285794408499 (Owner)`
 }
